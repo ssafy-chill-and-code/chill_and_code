d49a6c6a5f1068df986c57fb78e9304e8921f24f
juuni413 <wnsawol@gmail.com>
2025-11-27 23:44:50 +0900

    WIP: 일정 기본 CRUD 개발 중

--- backend/src/main/java/com/ssafy/chillandcode/controller/ScheduleController.java
+++ backend/src/main/java/com/ssafy/chillandcode/controller/ScheduleController.java
@@ -1,10 +1,90 @@
 package com.ssafy.chillandcode.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.chillandcode.model.dto.Schedule;
+import com.ssafy.chillandcode.model.service.ScheduleService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/schedules")
 public class ScheduleController {
+
+	@Autowired
+	private ScheduleService scheduleService;
+
+	// 일정 생성
+	@PostMapping
+	@Operation(summary = "일정 등록", description = "로그인된 사용자의 userId를 세션에서 가져와 새 일정을 생성합니다.")
+	public ResponseEntity<String> insertSchedule(@RequestBody Schedule schedule, HttpSession session) {
+		long userId = (Long) session.getAttribute("userId");
+		schedule.setUserId(userId);
+
+		boolean result = scheduleService.insertSchedule(schedule);
+
+		if (!result) {
+			return new ResponseEntity<>("일정 등록에 실패했습니다.", HttpStatus.BAD_REQUEST);
+		}
+
+		return new ResponseEntity<>("일정이 등록되었습니다.", HttpStatus.CREATED);
+	}
+
+	// 일정 조회
+	@GetMapping
+	@Operation(summary = "월별 일정 조회", description = "월 정보(YYYY-MM)를 Query Parameter로 받아 해당 사용자의 월별 일정을 조회합니다.")
+	public ResponseEntity<?> selectScheduleByMonth(@RequestParam String month, HttpSession session) {
+		Long userId = (Long) session.getAttribute("userId");
+
+		List<Schedule> result = scheduleService.selectScheduleByMonth(userId, month);
+
+		return new ResponseEntity<List<Schedule>>(result, HttpStatus.OK);
+	}
+
+	// 일정 수정
+	@PatchMapping("/{scheduleId}")
+	@Operation(summary = "일정 수정", description = "scheduleId에 해당하는 일정을 수정합니다. 해당 일정의 소유자인지 userId로 확인합니다.")
+	public ResponseEntity<String> updateSchedule(@PathVariable long scheduleId, @RequestBody Schedule schedule,
+			HttpSession session) {
+
+		long userId = (Long) session.getAttribute("userId");
+		schedule.setScheduleId(scheduleId);
+		schedule.setUserId(userId);
+		
+		boolean result = scheduleService.updateSchedule(userId, scheduleId, schedule);
+
+		if (!result) {
+			return new ResponseEntity<String>("일정 수정에 실패했습니다.", HttpStatus.BAD_REQUEST);
+		}
+
+		return new ResponseEntity<String>("일정이 수정되었습니다.", HttpStatus.OK); //size == 0이어도 OK
+	}
+
+	@DeleteMapping("/{scheduleId}")
+	@Operation(summary = "일정 삭제", description = "scheduleId에 해당하는 일정을 삭제합니다. 로그인한 사용자 본인의 일정만 삭제 가능합니다.")
+	public ResponseEntity<String> deleteSchedule(@PathVariable long scheduleId, HttpSession session) {
+		Long userId = (Long) session.getAttribute("userId");
+		boolean result = scheduleService.deleteSchedule(userId, scheduleId);
+
+		if (!result) {
+			return new ResponseEntity<String>("일정 삭제에 실패했습니다.", HttpStatus.BAD_REQUEST);
+		}
+
+		return new ResponseEntity<String>("일정이 삭제되었습니다.", HttpStatus.OK);
+	}
 
 }
--- backend/src/main/java/com/ssafy/chillandcode/model/dao/ScheduleDao.java
+++ backend/src/main/java/com/ssafy/chillandcode/model/dao/ScheduleDao.java
@@ -1,8 +1,27 @@
 package com.ssafy.chillandcode.model.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.ssafy.chillandcode.model.dto.Schedule;
 
 @Mapper
 public interface ScheduleDao {
-
+	
+	//일정 생성
+	int insertSchedule(Schedule schedule);
+	
+	//일정 조회
+	List<Schedule> selectScheduleByMonth(
+			@Param("userId") long userId, @Param("month") String month);
+	
+	//일정 수정
+	int updateSchedule(
+			@Param("userId") long userId, @Param("scheduleId") long scheduleId, Schedule schedule);
+	
+	//일정 삭제
+	int deleteSchedule(
+			@Param("userId") long userId, @Param("scheduleId") long scheduleId);
 }
