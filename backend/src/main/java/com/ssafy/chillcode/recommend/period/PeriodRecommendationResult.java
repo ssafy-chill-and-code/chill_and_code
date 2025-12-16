12f84d514d5219509cfdedc63adf6d09d8d76f5d
juuni413 <wnsawol@gmail.com>
2025-12-17 02:07:36 +0900

    Feat: 기간 추천 scoring 엔진 및 결과 모델 분리 구현

---
+++ backend/src/main/java/com/ssafy/chillcode/recommend/period/PeriodRecommendationResult.java
@@ -0,0 +1,54 @@
+package com.ssafy.chillcode.recommend.period;
+
+import java.util.List;
+
+public class PeriodRecommendationResult {
+
+	PeriodCandidate primary; // TOP1
+	String primaryReason; // 선택 이유
+
+	List<PeriodCandidate> alternatives; // Secondary options
+
+	public PeriodRecommendationResult() {
+
+	}
+
+	public PeriodRecommendationResult(PeriodCandidate primary, String primaryReason,
+			List<PeriodCandidate> alternatives) {
+		super();
+		this.primary = primary;
+		this.primaryReason = primaryReason;
+		this.alternatives = alternatives;
+	}
+
+	public PeriodCandidate getPrimary() {
+		return primary;
+	}
+
+	public void setPrimary(PeriodCandidate primary) {
+		this.primary = primary;
+	}
+
+	public String getPrimaryReason() {
+		return primaryReason;
+	}
+
+	public void setPrimaryReason(String primaryReason) {
+		this.primaryReason = primaryReason;
+	}
+
+	public List<PeriodCandidate> getAlternatives() {
+		return alternatives;
+	}
+
+	public void setAlternatives(List<PeriodCandidate> alternatives) {
+		this.alternatives = alternatives;
+	}
+
+	@Override
+	public String toString() {
+		return "PeriodRecommendationResult [primary=" + primary + ", primaryReason=" + primaryReason + ", alternatives="
+				+ alternatives + "]";
+	}
+
+}
